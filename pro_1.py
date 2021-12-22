78c37225d97bb05857273bc410a76aaa97fec074
RupaliPohane <pohane.rupali88@gmail.com>
2021-12-22 16:48:43 +0530

    first

---
+++ pro_1.py
@@ -0,0 +1,7 @@
+# Write a program to add and subtract two numbers
+x = 6
+y = 7
+addition = x + y
+subtraction = x - y
+print(addition)
+print(subtraction)
---
+++ pro_2.py
@@ -0,0 +1,7 @@
+# Write a program to multiply and divide two numbers
+x = 577
+y = 654
+Sum = x * y
+Div = x / y
+print(Sum)
+print(Div)
